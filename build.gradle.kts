8c0497ec4adc1e58b79c1f0efa5432d830f52062
kyle.koi <kyle@koi.security>
2025-06-09 09:57:56 +0300

    Add project files

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.17.3"
+}
+
+group = "com.pond"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    version.set("2023.2")         // Use IntelliJ version you're targeting
+    type.set("IC")                // "IC" for Community, "IU" for Ultimate
+    plugins.set(listOf("java"))   // Required plugins
+}
+
+tasks {
+    patchPluginXml {
+        version.set(project.version.toString())
+        sinceBuild.set("232")
+        untilBuild.set("241.*")
+    }
+
+    buildSearchableOptions {
+        enabled = false  // Optional: speeds up build
+    }
+}
